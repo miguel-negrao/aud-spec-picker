52670aeecec7af450232543de4b6f16cb19061a5
Miguel Negrão <miguel.negrao@friendlyvirus.org>
2013-01-15 22:13:38 +0000

    update

--- src/Main.hs
+++ src/Main.hs
@@ -3,27 +3,10 @@ module Main where
 import Text.SSV
 import Data.List
 import Data.Ord
-import System.Environment
-
-parseNum::String -> Double
-parseNum numString = read numString
+import System.Console.CmdArgs
 
 absRat::Double -> Double
 absRat x = if x < 1 then 1/x else x
-
-prefix:: String
-prefix = "/Volumes/Data/tracks/Matilde Florest Drone/10 min";
-
-paths :: [String]
-paths = [
-        prefix++"/10_annadale.txt",
-        prefix++"/10_lower_crescent.txt",
-        "/Volumes/Data/tracks/Matilde Florest Drone/CAIXAS 041212/DR-100_0461.txt",
-        "/Volumes/Data/tracks/Matilde Florest Drone/CAIXAS 041212/DR-100_0462.txt",
-        prefix++"/10_sunnyside.txt",
-        prefix++"/10_lockview.txt",
-        prefix++"/10_stranmillis.txt"
-        ]
 
 tabFormat :: SSVFormat
 tabFormat = SSVFormat {
@@ -38,7 +21,7 @@ tabFormat = SSVFormat {
     ssvFormatQuoteRight = '"' } }
     
 parseFromAudacity::String -> [[Double]]
-parseFromAudacity string =  map (map parseNum) x
+parseFromAudacity string =  map (map read) x
         where _:x = readSSV tabFormat string
         
 getFreqs:: Double -> [[Double]] -> [Double]
@@ -54,23 +37,24 @@ getFreqs delta xs = similarRemoved
 toCSV::[Double] -> String
 toCSV xs = foldl (++) "" $ map (\x -> (show x) ++ "\n" ) xs
 
-processFile :: Double -> Int -> String -> IO ()
-processFile delta numFreqs path = do
-        file <- readFile (path)
-        let result = toCSV $ sort $ take numFreqs $ ((getFreqs delta) . parseFromAudacity) file
-        let writePath = (reverse $ drop 4 (reverse path)) ++ "-selected.txt"   
-        writeFile writePath result
-        putStr result 
+processFile :: Double -> Int -> String -> String -> IO ()
+processFile delta numFreqs inputPath outputPath = do
+        file <- readFile (inputPath)
+        let result = toCSV $ sort $ take numFreqs $ ((getFreqs delta) . parseFromAudacity) file 
+        writeFile outputPath result
+        --putStr result 
+        
+data Args = Args { delta::Double, numFreqs::Int, inPath::FilePath, outPath::FilePath } deriving (Show, Data, Typeable)
+
+exampleArgs :: Args
+exampleArgs = Args { 
+        delta = 1.2 &= help "exclude frequencies near selected ones in range of delta",
+        numFreqs = 18 &= help "number of frequencies",
+        inPath = def &= argPos 0 &= typFile,
+        outPath = def &= argPos 1 &= typFile
+        } &= summary "Parse audacity spectrum analysis files"        
         
 main::IO()
-main = sequence_ $ map (processFile 1.2 18) paths
---main = processFile 1.2 18 (paths !! 0)
---main = main2
-
-main2 = do
-        delta:numFreqs:path:_ <- getArgs
-        processFile (parseNum delta) (read numFreqs) path
-        
-main3 = do
-        delta:numFreqs:_ <- getArgs
-        sequence_ $ map (processFile (read delta) (read numFreqs) ) paths
+main = do
+        args <- cmdArgs exampleArgs 
+        processFile (delta args) (numFreqs args) (inPath args) (outPath args)
